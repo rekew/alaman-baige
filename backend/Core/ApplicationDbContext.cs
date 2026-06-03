200dc10f075ec30f0bd581040935b5359997016e
MBFG9000 <dobridrug111@gmail.com>
2026-06-04 00:06:12 +0500

    hotfix:phone-number-unique-constraint

--- backend/Core/ApplicationDbContext.cs
+++ backend/Core/ApplicationDbContext.cs
@@ -13,5 +13,13 @@ public class ApplicationDbContext : DbContext
 
     public DbSet<User> Users { get; set; }
     public DbSet<RefreshToken> RefreshTokens { get; set; }
-    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>()
+            .HasIndex(user => user.PhoneNumber)
+            .IsUnique();
+    }
 }
