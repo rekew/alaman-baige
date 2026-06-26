97f5edf129922ec41b22177cc18dd2d9805f6604
Rustem Amirkhanuly <musulmazhamza@gmail.com>
2026-06-26 23:55:48 +0500

    feat: upload horse image

--- frontend/src/pages/admin-tables/horses/api.ts
+++ frontend/src/pages/admin-tables/horses/api.ts
@@ -1,16 +1,31 @@
 import { api } from "@/app/api";
 
 export const getTable = async () => {
-    try{
+    try {
         const res = await api.get("/admin/tables/horse");
 
-        if(res.status == 204){
+        if (res.status == 204) {
             return []
         }
 
         return res.data;
     }
-    catch(error){
+    catch (error) {
+        console.error(error);
+    }
+}
+
+export const createHorse = async (formData: FormData) => {
+    try {
+        const res = await api.post("/admin/tables/horse", formData, {
+            headers: {
+                "Content-Type": "multipart/form-data"
+            }
+        });
+
+        return res.data;
+    }
+    catch (error) {
         console.error(error);
     }
 }
